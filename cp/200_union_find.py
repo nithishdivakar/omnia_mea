25e3cdd9e4653f982de826c1123949af4afa2878
Nithish Divakar <nithishdivakar@users.noreply.github.com>
2024-11-05 19:07:01 +0530

    Create 200_union_find.py

---
+++ cp/200_union_find.py
@@ -0,0 +1,30 @@
+class DSU:
+  def __init__(self):
+    self.parent = {}
+    self.rank = {}
+
+  def find(self, x):
+    if x not in self.parent:
+      # new node
+      self.parent[x] = x
+      self.rank[x] = 0
+      
+    if self.parent[x] != x:
+      # path compression
+      self.parent[x] = self.find(self.parent[x])
+    return self.parent[x]
+
+  def union(self, x, y):
+    root_x = self.find(x)
+    root_y = self.find(y)
+  
+    if root_x != root_y:
+      if self.rank[root_x] < self.rank[root_y]:
+        root_x, root_y = root_y, root_x 
+
+      self.parent[root_y] = root_x
+
+      if self.rank[root_x] == self.rank[root_y]:
+        self.rank[root_x] += 1
+      return True
+    return False
