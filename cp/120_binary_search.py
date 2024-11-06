4b2d0cb92efb3178d4ed08189e901fb37aadf2c1
Nithish Divakar <nithishdivakar@users.noreply.github.com>
2024-11-06 19:51:18 +0530

    Update 120_binary_search.py

--- cp/120_binary_search.py
+++ cp/120_binary_search.py
@@ -1,12 +1,39 @@
-L = 0
-R = N-1
+# I.
+# Binary search pattern when we are searching for a target
+# in an ordered array with element w.r.to target like                                                     
+#       ┌───────────┬───┬────────────┐      
+#       │     <     │ = │     >      │      
+#       └───────────┴───┴────────────┘                                  
+
+L, R = 0, N-1
 
 while L <= R:
   mid = L + (R-L)//2
   if a[mid] == target:
     return mid
-  if a[mid] < target:
-    L = mid + 1
-  else:
+  if a[mid] > target:
     R = mid - 1
+  else:
+    L = mid + 1
 return -1
+
+
+# II.
+# Binary search pattern when we are searching for
+# the first/smallest valid position when the array is like                                   
+#   ┌───┬───┬───┬───┬───┬───┬───┬───┐      
+#   │ f │ f │ f │ t │ t │ t │ t │ t │     
+#   └───┴───┴───┴───┴───┴───┴───┴───┘   
+#                 └── ans 
+
+L, R = 0, N-1
+ans = -1
+
+while L <= R:
+  mid = L + (R-L)//2
+  if is_valid(mid):
+    ans = mid
+    R = mid - 1
+  else:
+    L = mid + 1
+return ans
